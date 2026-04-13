cbff7309e5de4bc07b1be0c1b87566768e100e06
Pinsterpan <francisho212@gmail.com>
2026-04-13 19:59:02 +0800

    1

---
+++ Python/1.TwoSume.py
@@ -0,0 +1,12 @@
+""" 
+This is the start of my Python practice
+I've grown a lot in the past few months
+So proud of myself 
+"""
+class Solution:
+    def twoSum(self,nums,target):
+        hash={}
+        for i in range(len(nums)):
+            if target-nums[i] in hash
+              return [hash[target-nums[i]],i]
+            hash[nums[i]]=i
