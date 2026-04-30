50450d2e0b7c35924db8fa527f6fbf70073676b0
Pinsterpan <francisho212@gmail.com>
2026-04-30 22:55:01 +0800

    8: low-level :(

---
+++ Python/8.StringToInteger.py
@@ -0,0 +1,22 @@
+#to be improved...
+class Solution(object):
+    def myAtoi(self, s):
+        n=len(s)
+        i=0
+        result=0
+        sign=1
+        while i<n and s[i]==" ":
+            i+=1
+        if i<n and (s[i]=="+" or s[i]=="-"):
+            sign = 1 if s[i]=="+" else -1
+            i+=1
+        while i<n and s[i]>="0" and s[i]<="9":
+            digit=int(s[i])
+            result=10*result+digit
+            i+=1
+        result=sign*result
+        if result>2**31-1:
+            return 2**31-1
+        if result<-2**31:
+            return -2**31
+        return result
