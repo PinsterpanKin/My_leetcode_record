aa7f7139116b427946853fb05a958333df64a1cd
Pinsterpan <francisho212@gmail.com>
2026-09-04 12:40:31 +0800

    24

--- Python/24.SwapNodesInPairs.py
+++ Python/24.SwapNodesInPairs.py
@@ -1,3 +1,24 @@
+class OriginalSolution:
+    def swapPairs(self, head):
+        if not head or not head.next:
+            return head
+
+        current = head
+        head = current.next
+        last_node = current
+
+        while current and current.next:
+            if current.next != head:
+                last_node.next = current.next
+                last_node = current
+            
+            next_node = current.next
+            current.next = next_node.next
+            next_node.next = current
+            current = current.next
+
+        return head
+        
 class Solution:
     def swapPairs(self, head):
         if not head or not head.next:
