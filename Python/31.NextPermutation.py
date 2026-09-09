f9a9bcb9704e1d47eeaec43425215ee9bb162c21
Pinsterpan <francisho212@gmail.com>
2026-09-09 16:17:13 +0800

    31

--- Python/31.NextPermutation.py
+++ Python/31.NextPermutation.py
@@ -7,11 +7,13 @@ class Solution:
         i = n - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
+        
         if i >= 0:
             j = n - 1
             while nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
+        
         self.reverse(nums, i + 1, n - 1)
 
     def reverse(self, nums: List[int], start: int, end: int) -> None:
