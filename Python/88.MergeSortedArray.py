443e4f034ea440a17fcf4c0c589dca7346f33ccc
Pinsterpan <francisho212@gmail.com>
2026-09-17 21:29:10 +0800

    88

---
+++ Python/88.MergeSortedArray.py
@@ -0,0 +1,32 @@
+class Solution:
+    #space complexity O(m+n) time complexity O(m+n)
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2[:n])
+
+class ClassicSolution:
+    #space complexity O(1) time complexity O(m+n)
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+        
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        
+        # If there are remaining elements in nums2, copy them over
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
