faa5db7dea14fa2e684c281039a650bc240da952
Pinsterpan <francisho212@gmail.com>
2026-09-17 21:02:35 +0800

    83

---
+++ Python/83.RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,13 @@
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
